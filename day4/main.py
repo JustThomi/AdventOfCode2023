9beefc53ee97b6ff321c5305dfa63846a87dbb26
Thomas <t.wild155@gmail.com>
2023-12-07 23:20:19 +0200

    forgot to commit

--- day4/main.py
+++ day4/main.py
@@ -1,5 +1,6 @@
 import re
 
+
 def findWinnings():
     with open('./puzzle4/input.txt') as f:
         lines = f.readlines()
@@ -8,12 +9,15 @@ def findWinnings():
         for line_id, line in enumerate(lines):
             # clean lines
             spaceless = re.sub(r'\s+', ' ', line)
-            clean_lines = spaceless.split('Card ' + str(line_id + 1) + ': ')[1].strip()
+            clean_lines = spaceless.split(
+                'Card ' + str(line_id + 1) + ': ')[1].strip()
             separate_lines = clean_lines.split('|')
 
             # separate winning numbers and your numbers
-            winning_numbers = [int(i) for i in separate_lines[0].split(' ') if i != '']
-            your_numbers = [int(i) for i in separate_lines[1].split(' ') if i != '']
+            winning_numbers = [int(i)
+                               for i in separate_lines[0].split(' ') if i != '']
+            your_numbers = [int(i)
+                            for i in separate_lines[1].split(' ') if i != '']
 
             total_matches = 0
             # check total numbers
@@ -23,49 +27,67 @@ def findWinnings():
                         total_matches = 1
                     else:
                         total_matches *= 2
-            
+
             sum += total_matches
-    
+
     return sum
 
-def totalCards():
+
+def storeCards():
     with open('./day4/input.txt') as f:
         lines = f.readlines()
         cards = dict()
-        copy_cards = dict()
-        sum = 0
 
         # separate cards
         for line_id, line in enumerate(lines):
             # clean lines
             spaceless = re.sub(r'\s+', ' ', line)
-            clean_lines = spaceless.split('Card ' + str(line_id + 1) + ': ')[1].strip()
+            clean_lines = spaceless.split(
+                'Card ' + str(line_id + 1) + ': ')[1].strip()
             separate_lines = clean_lines.split('|')
 
             # separate winning numbers and your numbers
-            winning_numbers = [int(i) for i in separate_lines[0].split(' ') if i != '']
-            your_numbers = [int(i) for i in separate_lines[1].split(' ') if i != '']
-            
+            winning_numbers = [int(i)
+                               for i in separate_lines[0].split(' ') if i != '']
+            your_numbers = [int(i)
+                            for i in separate_lines[1].split(' ') if i != '']
+
             # check total numbers
             total_matches = 0
             for nr in your_numbers:
                 if nr in winning_numbers:
                     total_matches += 1
-            
+
             # create dict
-            cards[line_id] = [winning_numbers, your_numbers, total_matches]
-            
-        
-        # count copies
-        for key, value in cards.items():
-            print(key, value)
-        
-    
-    return sum
+            cards[line_id + 1] = [winning_numbers, your_numbers, total_matches]
+
+    return cards
 
 
-def countCards(cards, copy, sum):
-    pass
+def countCards(cards, copies, s):
+    copy = []
+    final_sum = s
+
+    for item in copies:
+        total = item[1]
+
+        if total > 0:
+            for i in range(1, total + 1):
+                if item[0] + i in cards.keys():
+                    copy.append([item[0] + i, cards[item[0] + i][2]])
+
+    final_sum += len(copy)
+
+    if len(copy) > 0:
+        final_sum = countCards(cards, copy, final_sum)
+
+    return final_sum
+
 
 if __name__ == "__main__":
-    print(totalCards())
+    cards = storeCards()
+    list_cards = [[key, value[2]] for key, value in cards.items()]
+
+    sum = len(cards.keys())
+
+    print(countCards(cards, list_cards, sum))
